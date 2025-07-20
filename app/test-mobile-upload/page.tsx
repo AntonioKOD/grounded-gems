9e459b8c93d1f10815bf5bffed1c4dde7013deb9
Antonio Kodheli <antonio_kodheli@icloud.com>
2025-07-19 21:38:18 -0400

    new changes

--- app/test-mobile-upload/page.tsx
+++
@@ -1,9 +0,0 @@
-import MobilePostForm from "@/components/post/mobile-post-form"
-
-export default function TestMobileUploadPage() {
-  return (
-    <div className="min-h-screen">
-      <MobilePostForm />
-    </div>
-  )
-} 
