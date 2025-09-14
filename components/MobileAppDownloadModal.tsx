ea4f6f0a020b3f5175c730d791910b2d4030e506
Antonio Kodheli <antonio_kodheli@icloud.com>
2025-09-14 16:48:23 -0400

    added modal for ios download

--- components/MobileAppDownloadModal.tsx
+++ components/MobileAppDownloadModal.tsx
@@ -33,25 +33,20 @@ export default function MobileAppDownloadModal({
   const handleDownload = () => {
     onDownload()
     
-    if (isIOS) {
-      // Try to open the app if installed, otherwise open App Store
-      const appUrl = `sacavia://open` // Custom URL scheme
-      const appStoreUrl = APP_STORE_URL
-      
-      // Try to open the app first
-      const startTime = Date.now()
-      window.location.href = appUrl
-      
-      // If the app doesn't open within 2 seconds, redirect to App Store
-      setTimeout(() => {
-        if (Date.now() - startTime < 2000) {
-          window.location.href = appStoreUrl
-        }
-      }, 2000)
-    } else {
-      // For Android or other platforms, open App Store
-      window.open(APP_STORE_URL, '_blank')
-    }
+    // Try to open the app if installed, otherwise open App Store
+    const appUrl = `sacavia://open` // Custom URL scheme
+    const appStoreUrl = APP_STORE_URL
+    
+    // Try to open the app first
+    const startTime = Date.now()
+    window.location.href = appUrl
+    
+    // If the app doesn't open within 2 seconds, redirect to App Store
+    setTimeout(() => {
+      if (Date.now() - startTime < 2000) {
+        window.location.href = appStoreUrl
+      }
+    }, 2000)
   }
 
   const handleClose = () => {
@@ -84,50 +79,27 @@ export default function MobileAppDownloadModal({
                 Download our mobile application.
               </h2>
               <p className="text-white/80 text-sm mt-2 leading-relaxed">
-                Download Sacavia mobile app for iOS & Android to discover amazing places and connect with your community.
+                Download Sacavia mobile app for iOS to discover amazing places and connect with your community.
               </p>
             </div>
             
-            <div className="flex items-center justify-center gap-3">
+            <div className="flex items-center justify-center">
               {/* App Store Button */}
               <a 
                 href={APP_STORE_URL}
                 onClick={handleDownload}
-                className="flex items-center justify-between bg-white rounded-[10px] p-2 shadow-lg hover:shadow-xl transition-all duration-200 hover:scale-105"
+                className="flex items-center justify-between bg-white rounded-[10px] p-3 shadow-lg hover:shadow-xl transition-all duration-200 hover:scale-105 min-w-[200px]"
               >
-                <div className="w-6 h-6 flex items-center justify-center">
-                  <svg width="24" height="24" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <div className="w-7 h-7 flex items-center justify-center">
+                  <svg width="28" height="28" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M26.5058 27.625C25.33 29.3817 24.0833 31.0958 22.185 31.1242C20.2866 31.1667 19.6775 30.005 17.5241 30.005C15.3566 30.005 14.6908 31.0958 12.8916 31.1667C11.0358 31.2375 9.6333 29.2967 8.4433 27.5825C6.0208 24.0833 4.16497 17.6375 6.6583 13.3025C7.8908 11.1492 10.1008 9.78916 12.495 9.74666C14.3083 9.71833 16.0366 10.9792 17.1558 10.9792C18.2608 10.9792 20.3575 9.46333 22.5533 9.68999C23.4741 9.73249 26.0525 10.0583 27.71 12.495C27.5825 12.58 24.6358 14.3083 24.6641 17.8925C24.7066 22.1708 28.4183 23.6017 28.4608 23.6158C28.4183 23.715 27.8658 25.6558 26.5058 27.625ZM18.4166 4.95833C19.4508 3.78249 21.165 2.88999 22.5816 2.83333C22.7658 4.49083 22.1 6.16249 21.1083 7.35249C20.1308 8.55666 18.5158 9.49166 16.9291 9.36416C16.7166 7.73499 17.51 6.03499 18.4166 4.95833Z" fill="#000" />
                   </svg>
                 </div>
-                <div className="flex flex-col ml-2">
+                <div className="flex flex-col ml-3">
                   <span className="text-[#4ecdc4] text-xs font-semibold">Download from</span>
                   <span className="text-black text-sm font-bold">App Store</span>
                 </div>
               </a>
-              
-              {/* Google Play Button (for Android users) */}
-              {!isIOS && (
-                <a 
-                  href="#"
-                  onClick={(e) => {
-                    e.preventDefault()
-                    // For now, redirect to App Store since we only have iOS app
-                    handleDownload()
-                  }}
-                  className="flex items-center justify-between bg-[#4ecdc4] rounded-[10px] p-2 shadow-lg hover:shadow-xl transition-all duration-200 hover:scale-105"
-                >
-                  <div className="w-6 h-6 flex items-center justify-center">
-                    <svg width="24" height="24" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M4 28.9958V4.9125C4 4.07667 4.48167 3.34 5.19 3L19.1442 16.9542L5.19 30.9083C4.48167 30.5542 4 29.8317 4 28.9958ZM23.5642 21.3742L8.32083 30.1858L20.3483 18.1583L23.5642 21.3742ZM28.31 15.2683C28.7917 15.6508 29.1458 16.2458 29.1458 16.9542C29.1458 17.6625 28.8342 18.2292 28.3383 18.6258L25.0942 20.4958L21.5525 16.9542L25.0942 13.4125L28.31 15.2683ZM8.32083 3.7225L23.5642 12.5342L20.3483 15.75L8.32083 3.7225Z" fill="white" />
-                    </svg>
-                  </div>
-                  <div className="flex flex-col ml-2">
-                    <span className="text-white/80 text-xs font-semibold">Get it on</span>
-                    <span className="text-white text-sm font-bold">Google Play</span>
-                  </div>
-                </a>
-              )}
             </div>
           </div>
         </div>
